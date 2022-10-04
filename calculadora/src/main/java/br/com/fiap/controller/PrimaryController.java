3dd1c7b357616b51841e81995222080d3ade9a84
Luis <rm93025@fiap.com.br>
2022-10-04 12:09:15 -0300

    feat: metodo pra salvar log

--- calculadora/src/main/java/br/com/fiap/controller/PrimaryController.java
+++ calculadora/src/main/java/br/com/fiap/controller/PrimaryController.java
@@ -1,6 +1,6 @@
 package br.com.fiap.controller;
 
-import java.io.IOException;
+import java.sql.SQLException;
 
 import br.com.fiap.dao.LogDao;
 import br.com.fiap.model.Log;
@@ -18,8 +18,8 @@ public class PrimaryController {
     private double numero1;
     private String operacao;
 
-    public void digitarNumero(ActionEvent event) {
-        Button botao = (Button) event.getSource(); 
+    public void digitarNumero(ActionEvent event){
+        Button botao = (Button) event.getSource();
         String valorDoDisplay = display.getText();
         if (valorDoDisplay.equals("0")) valorDoDisplay = "";
         display.setText(valorDoDisplay + botao.getText());
@@ -28,10 +28,10 @@ public class PrimaryController {
     public void clear(){
         display.setText("0");
     }
-    
+
     public void delete(){
         String valorDoDisplay = display.getText();
-        valorDoDisplay = valorDoDisplay.substring(0, valorDoDisplay.length() - 1);
+        valorDoDisplay = valorDoDisplay.substring(0, valorDoDisplay.length()-1);
         display.setText(valorDoDisplay);
     }
 
@@ -39,12 +39,14 @@ public class PrimaryController {
         double valorDoDisplay = Double.valueOf(display.getText());
         double raiz = Math.sqrt(valorDoDisplay);
         display.setText(String.valueOf(raiz));
+        salvarLog(new Log(valorDoDisplay, 0, "raiz", raiz));
     }
 
     public void quadrado(){
         double valorDoDisplay = Double.valueOf(display.getText());
         double quadrado = Math.pow(valorDoDisplay, 2);
         display.setText(String.valueOf(quadrado));
+        salvarLog(new Log(valorDoDisplay, 0, "quadrado", quadrado));
     }
 
     public void operacao(ActionEvent event){
@@ -57,20 +59,25 @@ public class PrimaryController {
     public void calcular(){
         double numero2 = Double.valueOf(display.getText());
         double resultado = 0;
-        if(operacao.equals("+")) resultado = numero1 + numero2;
-        if(operacao.equals("-")) resultado = numero1 - numero2;
-        if(operacao.equals("X")) resultado = numero1 * numero2;
-        if(operacao.equals("/")) resultado = numero1 / numero2;
+        
+        if (operacao.equals("+")) resultado = numero1 + numero2;
+        if (operacao.equals("-")) resultado = numero1 - numero2;
+        if (operacao.equals("x")) resultado = numero1 * numero2;
+        if (operacao.equals("/")) resultado = numero1 / numero2;
         
         display.setText(String.valueOf(resultado));
-        try{
-            new LogDao().inserir(new Log(numero1, numero2, operacao, resultado));
-        }catch(SQLException e){
-            var alerta = new Alert(AlertType.ERROR);
-            alerta.setContentText("Nao foi possivel salvar a operacao no BD.");
-            alerta.show();
-        }
 
+        salvarLog(new Log(numero1, numero2, operacao, resultado));
+            
     }
 
+    private void salvarLog(Log log){
+        try{
+            new LogDao().inserir(log);
+        }catch(SQLException e){
+            var alerta = new Alert(AlertType.ERROR);
+            alerta.setContentText("Não foi possível salvar a operação no BD");
+            alerta.show();
+        }
+    }
 }
